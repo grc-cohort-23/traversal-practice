6281e24ce417cb113bb7f46b3bc4e37a2d3df5a5
JHud <219741840+JamHudson@users.noreply.github.com>
2026-06-02 16:55:59 -0700

    print odd nodes

--- src/TraversalPractice.java
+++ src/TraversalPractice.java
@@ -8,7 +8,11 @@ public class TraversalPractice {
    * @param node The root of the tree to print
    */
   public static void printOddNodes(Node<Integer> node) {
-
+    if (node == null) return;
+    printOddNodes(node.left);
+    printOddNodes(node.right);
+    // If Node is odd
+    if (node.value % 2 != 0) System.out.println(node.value);
   }
 
   /**
