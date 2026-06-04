f52da6d2f71b41c541dad4612b6d0cc59c08bb24
Nadia <239692631+nadia5129@users.noreply.github.com>
2026-06-03 23:35:27 -0700

    completed last question

--- src/TraversalPractice.java
+++ src/TraversalPractice.java
@@ -68,7 +68,14 @@ public class TraversalPractice {
    * @return the max value
    */
   public static int maxVal(Node<Integer> node) {
+    if(node == null) {
     return 0;
+  }  
+  int leftMax = maxVal(node.left);
+  int rightMax = maxVal(node.right);
+
+    return Math.max(node.value, Math.max(leftMax, rightMax));
+
   }
 
   /**
@@ -80,7 +87,13 @@ public class TraversalPractice {
    * @return The number of levels in the tree
    */
   public static int numLevels(Node<?> node) {
-    return 0;
+        if (node == null) {
+        return 0;
+    }
+    return 1 + Math.max(
+        numLevels(node.left),
+        numLevels(node.right)
+    );
   }
 
   public static void main(String[] args) {
@@ -94,7 +107,16 @@ public class TraversalPractice {
 
      // Replace the below line to create a tree 
      // as represented in the diagram above
-     Node<Integer> smallTree = null;
+     Node<Integer> smallTree = new Node<>(99,
+    new Node<>(45,
+
+        new Node<>(9, null, null),
+        new Node<>(5, null, null)),
+
+    new Node<>(82,
+      
+        null,
+        new Node<>(16, null, null)));
 
 
     /*
