3465d0146c67348c6ebbbd8b911c5dac4deef650
Samuel <174657033+SamuelM007@users.noreply.github.com>
2026-06-03 21:46:21 -0700

    printOddNodes, printNodesWithOneChild

--- src/TraversalPractice.java
+++ src/TraversalPractice.java
@@ -8,7 +8,12 @@ public class TraversalPractice {
    * @param node The root of the tree to print
    */
   public static void printOddNodes(Node<Integer> node) {
+    if(node == null) return;
 
+    printOddNodes(node.left);
+    printOddNodes(node.right);
+
+    if(node.value %2 == 1) System.out.println(node.value);
   }
 
   /**
@@ -19,7 +24,17 @@ public class TraversalPractice {
    * @param node The root of the tree to print
    */
   public static void printNodesWithOneChild(Node<?> node) {
-    
+    if(node == null) return;
+
+    if(node.left == null && node.right == null) return;
+
+    if(node.left == null || node.right == null){
+      System.out.println(node.value);
+    }
+
+    printNodesWithOneChild(node.left);
+    printNodesWithOneChild(node.right);
+
   }
 
     /**
@@ -31,7 +46,9 @@ public class TraversalPractice {
    * @return the sum 
    */
   public static int treeSum(Node<Integer> node) {
-    return 0;
+    if(node == null) return 0;
+
+    return 1 + treeSum(node.left) + treeSum(node.right);
   }
 
   /**
