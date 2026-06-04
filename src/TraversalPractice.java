56ec0f7c273f3d94f0b9029c1a0b60dd5fddcae2
Jessica H <jessicahebert75@gmail.com>
2026-06-03 18:27:07 -0700

    Implement odd node and single-child node traversals

--- src/TraversalPractice.java
+++ src/TraversalPractice.java
@@ -8,7 +8,15 @@ public class TraversalPractice {
    * @param node The root of the tree to print
    */
   public static void printOddNodes(Node<Integer> node) {
-
+    if (node == null){
+      return;
+    }
+    printOddNodes(node.left);
+    printOddNodes(node.right);
+    
+    if (node.value % 2 != 0){
+      System.out.println(node.value);
+      }
   }
 
   /**
@@ -19,6 +27,14 @@ public class TraversalPractice {
    * @param node The root of the tree to print
    */
   public static void printNodesWithOneChild(Node<?> node) {
+    if (node == null) return;
+
+    if(node.left == null &  node.right != null|| node.left != null & node.right == null){
+      System.out.println(node.value);
+    }
+    printNodesWithOneChild(node.left);
+    printNodesWithOneChild(node.right);
+
     
   }
 
