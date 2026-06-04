c3ffce166402a829f4170ed458033495bce5fcaf
Nadia <239692631+nadia5129@users.noreply.github.com>
2026-06-03 23:13:15 -0700

    completed third question

--- src/TraversalPractice.java
+++ src/TraversalPractice.java
@@ -50,7 +50,12 @@ public class TraversalPractice {
    * @return the sum 
    */
   public static int treeSum(Node<Integer> node) {
-    return 0;
+       if (node == null) {
+        return 0;
+    }
+    return node.value
+         + treeSum(node.left)
+         + treeSum(node.right);
   }
 
   /**
