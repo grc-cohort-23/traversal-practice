d5b24d7042d157c6fd0f2f8b00f7a67546109510
sodatax <h.nacanaynay@gmail.com>
2026-06-03 19:18:24 -0700

    finished printNodesWithOneChild and treeSum

--- src/TraversalPractice.java
+++ src/TraversalPractice.java
@@ -24,7 +24,12 @@ public class TraversalPractice {
    * @param node The root of the tree to print
    */
   public static void printNodesWithOneChild(Node<?> node) {
-    
+    if(node==null) return;
+    if(node.left==null && node.right!=null || node.right==null && node.left!=null){
+      System.out.println(node.value);
+    }
+    printNodesWithOneChild(node.left);
+    printNodesWithOneChild(node.right);
   }
 
     /**
@@ -36,7 +41,9 @@ public class TraversalPractice {
    * @return the sum 
    */
   public static int treeSum(Node<Integer> node) {
-    return 0;
+    if(node==null) return 0;
+    return node.value + treeSum(node.left) + treeSum(node.right);
+
   }
 
   /**
